6216a6c1a5a41382824853b86be3b9064fa09ec6
MD Ahasan Habib <ahasanh303@gmail.com>
2026-06-05 22:11:00 +0600

    Update sync.js

--- functions/api/sync.js
+++ functions/api/sync.js
@@ -1,12 +1,20 @@
 export async function onRequestGet(context) {
-      const { env } = context;
-        const messagesRaw = await env.CHAT_STORE.get("global_chat_stream");
-          const typingRaw = await env.CHAT_STORE.get("global_typing_stream");
-            
-              return new Response(JSON.stringify({
-                  messages: messagesRaw ? JSON.parse(messagesRaw) : [],
-                      typing: typingRaw ? JSON.parse(typingRaw) : {}
-                        }), { headers: { "Content-Type": "application/json" } });
-                        }
-                        
+  const { env } = context;
+
+  const messagesRaw = await env.CHAT_STORE.get("global_chat_stream");
+  const typingRaw = await env.CHAT_STORE.get("global_typing_stream");
+
+  return new Response(
+    JSON.stringify({
+      messages: messagesRaw ? JSON.parse(messagesRaw) : [],
+      typing: typingRaw ? JSON.parse(typingRaw) : {}
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}
+
 }
